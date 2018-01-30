826229783310cee5e21de359456cc7cac6e2ca53
php 11 <php.11@genio.cec.ime.usp.br>
2018-01-30 21:46:15 -0200

    first commit

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Saudação</title>
+  </head>
+  <body>
+
+    <?php for ($i=0; $i < 50; $i++): ?>
+      <h1>Olá ! </h1>
+    <?php endfor; ?>
+
+    @for($i=0; $i<50; $i++)
+      <h1>Olá Blame</h1>
+    @endfor;
+
+
+  </body>
+</html>
